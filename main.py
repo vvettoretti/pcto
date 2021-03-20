9e00c95d502fa571538a0d8f203a3c3c810f145e
vvettoretti <valeriovettoretti04@gmail.com>
2021-03-20 12:31:21 +0100

    New Output

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ class User:
         self.area3_total = self.user_styles[7] + self.user_styles[8]
         for style, count in self.user_styles.items():
             if style <= 4:
-                self.user_styles_percentage[style] = round(count * 100 / self.area1_total)
+                self.user_styles_percentage[style] = round(count * 100 / self.area1_total, 2)
             elif style >= 5 and style <= 6:
-                self.user_styles_percentage[style] = round(count * 100 / self.area2_total)
+                self.user_styles_percentage[style] = round(count * 100 / self.area2_total, 2)
             elif style >= 7:
-                self.user_styles_percentage[style] = round(count * 100 / self.area3_total)
+                self.user_styles_percentage[style] = round(count * 100 / self.area3_total, 2)
 
 
 def read_csv(f):
@@ -55,13 +55,18 @@ def read_csv(f):
 
 
 def write_csv(f, users):
-    with open(f, "w+") as csv_file:
+    with open(f, "w+", newline="") as csv_file:
         writer = csv.writer(csv_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(
+            ["Cognome", "Nome", "Mail", "Classe", "Stile 1%", "Stile 2%", "Stile 3%", "Stile 4%", "Stile 5%",
+             "Stile 6%", "Stile 7%", "Stile 8%"])
         for user in users:
-            to_write = [user.name]
+            # print(user.user_info)
+            to_write = [user.user_info["COGNOME"], user.user_info["NOME"], user.user_info["Indirizzo email"],
+                        "1" + user.user_info["CLASSE PRIMA "]]
             for key, value in user.user_styles_percentage.items():
-                to_write.append(key)
-                to_write.append(value)
+                # to_write.append(key)
+                to_write.append(str(value) + "%")
             writer.writerow(to_write)
 
 
@@ -70,6 +75,7 @@ users = []
 for file in files:
     read = read_csv(file)
     for row in read:
+        # print(row)
         users.append(User(row))
 
 write_csv("f.csv", users)
